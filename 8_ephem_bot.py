fecb4db62b877460915a2b7f5f6bab105b0844ca
SanuNak <sanu.nak.@gmail.com>
2021-12-05 15:04:56 +0300

    1.Поправил ошибки. 2. Улучшил логику игры. 3.Добавил пояснения

--- 8_ephem_bot.py
+++ 8_ephem_bot.py
@@ -1,30 +1,10 @@
 """
-Домашнее задание №1
-
-Использование библиотек: ephem
-1.
-Реализуйте в боте команду /wordcount которая считает слова в присланной фразе.
-Например на запрос /wordcount Привет как дела бот должен ответить: 3 слова. Не забудьте:
-Добавить проверки на пустую строку
-Как можно обмануть бота, какие еще проверки нужны?
-------------
-2.
-Реализуйте в боте команду, которая отвечает на вопрос
-“Когда ближайшее полнолуние?” Например /next_full_moon 2019-01-01. Чтобы узнать,
-когда ближайшее полнолуние, используйте ephem.next_full_moon(ДАТА)
------------
-3.
-Научите бота играть в города. Правила такие - внутри бота есть список городов, пользователь пишет /cities Москва и если
-в списке такой город есть, бот отвечает городом на букву "а" - "Альметьевск, ваш ход".
-Оба города должны удаляться из списка.
-Помните, с ботом могут играть несколько пользователей одновременно
-------------
-4.
+Домашнее задание №2 доп
+4. не сделал
 Научите бота выполнять основные арифметические действия с двумя числами:
 сложение, вычитание, умножение и деление. Если боту дать команду /calc 2-3, он должен ответить “-1”.
 Не забудьте обработать возможные ошибки во вводе: пробелы, отсутствие чисел, деление на ноль
 Подумайте, как можно сделать поддержку действий с тремя и более числами
-
 """
 import logging
 import ephem
@@ -46,8 +26,14 @@ PROXY = {
         'password': setting.PROXY_PASSWORD
     }
 }
-
-# Задание 1
+"""
+    Задание 1
+    Использование библиотек: ephem
+    Реализуйте в боте команду /wordcount которая считает слова в присланной фразе.
+    Например на запрос /wordcount Привет как дела бот должен ответить: 3 слова. Не забудьте:
+    Добавить проверки на пустую строку
+    Как можно обмануть бота, какие еще проверки нужны?
+"""
 def word_count(update, context):
     user_text = update.message.text
     if user_text.strip() != "/wordcount":
@@ -62,8 +48,12 @@ def word_count(update, context):
             update.message.reply_text(f'{cnt_word} слов')  # ответ бота
     else:
         update.message.reply_text(f'Ничего не введено')  # ответ бота
-
-# Задание 2
+"""
+    Задание 2
+    Реализуйте в боте команду, которая отвечает на вопрос
+    “Когда ближайшее полнолуние?” Например /next_full_moon 2019-01-01. Чтобы узнать,
+    когда ближайшее полнолуние, используйте ephem.next_full_moon(ДАТА)
+"""
 def next_full_moon(update, context):
     user_text = update.message.text
     now_data = dt.date.today()
@@ -71,60 +61,77 @@ def next_full_moon(update, context):
     update.message.reply_text(f'Ближайшее полнолуние {next_full_moon}')
 
 
-# Задание 3
+"""
+Задание 3
+Научите бота играть в города. Правила такие - внутри бота есть список городов, пользователь пишет /cities Москва и если
+в списке такой город есть, бот отвечает городом на букву "а" - "Альметьевск, ваш ход".
+Оба города должны удаляться из списка.
+Помните, с ботом могут играть несколько пользователей одновременно
+"""
 def game_cities(update, context):
-    cities_list =['Москва', 'Кировск', 'Анапа', 'Новый волочек']
+    cities_list =['Москва', 'Абакан', 'Новый волочек']
     if (context.user_data == {}) | (not context.user_data.get(update.message.chat.id)):
         context.user_data[update.message.chat.id] = {'bot_cities': cities_list,
-                                                     'cities_user': context.args}
-    else:
-        context.user_data[update.message.chat.id]['cities_user'].append(
-            " ".join(context.args).lower().strip(" ").capitalize()
-                                                                        )
+                                                     'former_bot_cities': [],
+                                                     'former_user_cities': []}
+    # Проверка. Юзер ничего не ввел
     if context.args == []:
         update.message.reply_text(f'Ничего не введено!')  # ответ бота
         return
-
+    # Проверка. Список бота пуст
     if context.user_data[update.message.chat.id]['bot_cities'] == []:
-        update.message.reply_text(f'Я здаюсь! Конец игры! \n Но могу начать заново')  # ответ бота
+        update.message.reply_text(f'Я сдаюсь! Конец игры! \nНо могу начать заново')  # ответ бота
         context.user_data[update.message.chat.id]['bot_cities'] = cities_list
-        context.user_data[update.message.chat.id]['cities_user'] = []
         return
 
-    user_city = context.user_data[update.message.chat.id]['cities_user'][-1]
-    user_cities = context.user_data[update.message.chat.id]['cities_user']
+    user_city = " ".join(context.args).lower().strip(" ").capitalize()
+    former_user_cities = context.user_data[update.message.chat.id]['former_user_cities']
     bot_cities = context.user_data[update.message.chat.id]['bot_cities']
+    former_bot_cities = context.user_data[update.message.chat.id]['former_bot_cities']
 
-    if user_city in bot_cities:
-        end_letter_user_cities = user_city[-1]
-        bot_cities.remove(user_city)
-        if bot_cities == []:
-            update.message.reply_text(f'Я здаюсь! Конец игры! \n Но могу начать заново')  # ответ бота
-            context.user_data[update.message.chat.id]['bot_cities'] = cities_list
-            context.user_data[update.message.chat.id]['cities_user'] = []
-            return
-        for bot_city in bot_cities:
-            bot_city = bot_city.lower()
-            if bot_city[0] == end_letter_user_cities:
-                update.message.reply_text(f'{bot_city.capitalize()}, ваш ход!')  # ответ бота
-                bot_cities.remove(bot_city.capitalize())
-                return
-            elif bot_city[0] != end_letter_user_cities:
-                continue
-            elif bot_cities[0].lower() == bot_city:
-                update.message.reply_text(f'Я здаюсь! Конец игры! \n Но могу начать заново')  # ответ бота
-                context.user_data[update.message.chat.id]['bot_cities'] = cities_list
-                context.user_data[update.message.chat.id]['cities_user'] = []
-                return
-            else:
-                update.message.reply_text(f'Я здаюсь! Конец игры!')  # ответ бота
-                context.user_data[update.message.chat.id]['bot_cities'] = cities_list
-                context.user_data[update.message.chat.id]['cities_user'] = []
-    elif user_city in user_cities:
+    # Проверка. Город юзера уже был задействован в игре
+    if (user_city in former_user_cities) | (user_city in former_bot_cities):
         update.message.reply_text(f'Такой город уже был')  # ответ бота
         return
+    # Проверка. Введенный город юзером отсутствует в списке бота
+    if user_city not in bot_cities:
+        update.message.reply_text(f'Такого города я не знаю, введи другой!')  # ответ бота
+        return
+
+    # Проверка. Город с неправильной первой буквой
+    if former_bot_cities !=[]:
+        if user_city[0].lower() != former_bot_cities[-1][-1]:
+            update.message.reply_text(f'Караул!!! Первая буква должна быть {former_bot_cities[-1][-1]}')  # ответ бота
+            return
+
+    # Если город юзера в списке, то начинаем выбирать подходищий город из списка Бота.
+    if user_city in bot_cities:
+        end_letter_user_cities = user_city[-1]
+        for bot_city in bot_cities:
+            bot_city = bot_city.lower()
+            # На случай если последний город из списка бота ввел пользователь. Здаемся, городов больше нет
+            if (len(bot_cities) == 1) & (bot_cities[0].lower() == user_city.lower()):
+                update.message.reply_text(f'Я сдаюсь! Конец игры! \nНо могу начать заново')  # ответ бота
+                context.user_data.clear()
+                return
+            # Основной If. Печатаем ответ если первая буква города Бота совпадает с первой буквой города юзера
+            if bot_city[0] == end_letter_user_cities:
+                if user_city.lower() == bot_city:
+                    continue
+                update.message.reply_text(f'{bot_city.capitalize()}, ваш ход!')  # ответ бота
+                former_bot_cities.append(bot_cities.pop(bot_cities.index(bot_city.capitalize())))
+                former_user_cities.append(bot_cities.pop(bot_cities.index(user_city)))
+                return
+            # На случай если очередной город из списка не подошел по первой букве
+            elif bot_city[0] != end_letter_user_cities:
+                continue
+            # На случай непредвиданного условия
+            else:
+                update.message.reply_text(f'Я сдаюсь! Конец игры! \nНо могу начать заново')  # ответ бота
+                context.user_data.clear()
+                return
     else:
-        update.message.reply_text(f'Такого города не знаю, введите другой')  # ответ бота
+        update.message.reply_text(f'Назовите еще город!')  # ответ бота
         return
 
 def greet_user(update, context):
